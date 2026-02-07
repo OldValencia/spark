ee6c2c849b5900af3df421d23fbc472e57266569
valiantsin_kandakou <valiantsin.kandakou@sonalake.com>
2026-02-07 21:32:26 +0100

    Fix logs, hotkey label and Passkey bug

--- src/main/java/io/loom/app/utils/LogSetup.java
+++ src/main/java/io/loom/app/utils/LogSetup.java
@@ -1,8 +1,12 @@
 package io.loom.app.utils;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
 import io.loom.app.config.AppPreferences;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -27,8 +31,29 @@ public class LogSetup {
 
         var timestamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
         var logFileName = "app-" + timestamp;
+
         System.setProperty("log.name", logFileName);
         System.setProperty("log.dir", LOGS_DIR);
+
+        reloadLogbackConfig();
+        System.out.println("Logs initialized at: " + new File(logsDir, logFileName + ".log").getAbsolutePath());
+    }
+
+    private static void reloadLogbackConfig() {
+        var context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        try {
+            var configurator = new JoranConfigurator();
+            configurator.setContext(context);
+            context.reset();
+
+            var configFile = LogSetup.class.getResource("/logback.xml");
+            if (configFile != null) {
+                configurator.doConfigure(configFile);
+            }
+        } catch (JoranException e) {
+            System.out.println("It's impossible to reload config " + e.getMessage());
+        }
     }
 
     static void rotateCefLog(File dir) {
